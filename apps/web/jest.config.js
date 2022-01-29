d840149caf3d0378b324d95228a272d3972c7948
Reece Rose <reecerose18@gmail.com>
2022-01-29 13:20:01 -0500

    Moved ui component testing to it's package instead of web. Fixed other test related issues

--- apps/web/jest.config.js
+++ apps/web/jest.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  collectCoverageFrom: [
-    '**/*.{js,jsx,ts,tsx}',
-    '!**/*.d.ts',
-    '!**/node_modules/**',
-  ],
+  ...require('config/jest-nextjs'),
   moduleNameMapper: {
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
     '^@/components/(.*)$': '<rootDir>/components/$1',
@@ -14,12 +10,4 @@ module.exports = {
   setupFiles: ['<rootDir>/.jest/setEnvVars.js'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setupTests.js'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
-  },
-  transformIgnorePatterns: [
-    '/node_modules/',
-    '^.+\\.module\\.(css|sass|scss)$',
-  ],
-  testEnvironment: 'jest-environment-jsdom',
 }
