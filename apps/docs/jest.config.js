4142e64afd8b1e2ef38ad47c3a55819d5cfe9b25
Reece Rose <reecerose18@gmail.com>
2022-01-29 13:23:24 -0500

    Copied web folder and renamed to docs

---
+++ apps/docs/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  ...require('config/jest-nextjs'),
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    '^@/layouts/(.*)$': '<rootDir>/layouts/$1',
+    '^@/shared/(.*)$': '<rootDir>/node_modules/ui/$1',
+  },
+  setupFiles: ['<rootDir>/.jest/setEnvVars.js'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setupTests.js'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+}
---
+++ apps/docs/pages/index.tsx
@@ -0,0 +1,13 @@
+import { SEO } from '@/shared/components';
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <SEO
+        title="CHANGE_ME"
+        description="CHANGE_ME"
+        siteURL={process.env.SITE_URL || 'https://localhost:3000'}
+      />
+    </div>
+  );
+}
