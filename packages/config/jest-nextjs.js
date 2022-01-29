36eb7b959a7fc72b72131121615533192b06ad71
Reece Rose <reecerose18@gmail.com>
2022-01-29 18:27:53 -0500

    Move to one build

--- packages/config/jest-nextjs.js
+++ packages/config/jest-nextjs.js
@@ -9,7 +9,7 @@ module.exports = {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
   transformIgnorePatterns: [
-    '/node_modules/(?!ui)(.*)',
+    '/node_modules/',
     '^.+\\.module\\.(css|sass|scss)$',
   ],
   testEnvironment: 'jest-environment-jsdom',
