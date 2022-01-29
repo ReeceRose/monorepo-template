b4fa1e2f41e974a49ab8be336eec7c19ca1c5b9f
Reece Rose <reecerose18@gmail.com>
2022-01-29 13:20:33 -0500

    Removed docs folder (temp)

--- apps/docs/next.config.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withTM = require('next-transpile-modules')(['ui']);
-
-module.exports = withTM({
-  reactStrictMode: true,
-  swcMinify: true,
-});
