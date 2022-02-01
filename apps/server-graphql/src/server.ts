89f795d53bb2c56a972b3789792bf7226b86c2de
Reece Rose <reecerose18@gmail.com>
2022-01-31 20:53:15 -0500

    Swapped apollo-server for express-graphql

--- apps/server-graphql/src/server.ts
+++ apps/server-graphql/src/server.ts
@@ -1,15 +1,20 @@
-import { ApolloServer } from 'apollo-server';
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+
 import { schema } from './schema';
 
-export const server = new ApolloServer({
-  schema,
-});
+const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
+
 const PORT = process.env.PORT || 3003;
 
-void server.listen(PORT, (error: unknown) => {
-  if (error) {
-    throw error;
-  }
-
+app.listen(PORT, () => {
   console.log(`🚀 GraphQL server started at https://localhost:${PORT}`);
 });
